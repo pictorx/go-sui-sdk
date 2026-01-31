5fdbedcc17b746717567e4f3b4d711f599e7af95
Immanuel Njuguna <njugunaimmanuel1@outlook.com>
2026-01-31 23:37:43 +0300

    Most of reading operations work

--- example/main.go
+++ example/main.go
@@ -6,17 +6,15 @@ import (
 	"log"
 	"time"
 
-	pb "github.com/pictorx/go-sui-sdk/sui_rpc_proto/generated" // Replace with your actual import path for the generated package (e.g., github.com/yourrepo/sui/rpc/v2)
+	gosuisdk "github.com/pictorx/go-sui-sdk"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
 func main() {
-	// Endpoint for Sui mainnet full node gRPC (adjust for testnet: fullnode.testnet.sui.io:443)
 	endpoint := "fullnode.testnet.sui.io:443"
 
-	// Dial the gRPC server with TLS (using system cert pool for verification)
-	conn, err := grpc.NewClient(
+	conn, err := grpc.Dial(
 		endpoint,
 		grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(nil, "")),
 	)
@@ -25,19 +23,112 @@ func main() {
 	}
 	defer conn.Close()
 
-	// Create clients for specific services (from your generated code)
-	ledgerClient := pb.NewLedgerServiceClient(conn) // For reading chain data like checkpoints, objects, transactions
-	// txExecClient := pb.NewTransactionExecutionServiceClient(conn) // For submitting/executing transactions
-	// Add other clients as needed, e.g., pb.NewMovePackageServiceClient(conn)
-
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
 
-	resp, err := ledgerClient.GetEpoch(ctx, &pb.GetEpochRequest{})
-	if err != nil {
-		log.Fatalf("GetEpoch failed: %v", err)
-	}
+	getEpoch(conn, ctx)
+	getObject(conn, ctx)
+	getTransaction(conn, ctx)
+	batchGetObjects(conn, ctx)
+	batchGetTransactions(conn, ctx)
+	getServiceInfo(conn, ctx)
+	getPackage(conn, ctx)
+	getFunction(conn, ctx)
+	getDatatype(conn, ctx)
+	listPackageVersions(conn, ctx)
+	getBalance(conn, ctx)
+	getCoinInfo(conn, ctx)
+	listBalances(conn, ctx)
+	listOwnedObjects(conn, ctx)
+	listDynamicFields(conn, ctx)
 
-	// Print the response (adjust based on your needs)
-	fmt.Printf("%+v\n", resp)
+}
+
+func response(r any, err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v", r)
+}
+
+func getEpoch(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.GetEpoch(conn, ctx)
+	response(resp, err)
+}
+
+func getObject(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.GetObject(conn, "0xe567f65413d10d585bbec909f46f11c5fc666061e1ede471b840f3d1cf25eaaa",
+		nil, ctx)
+	response(resp, err)
+}
+
+func getTransaction(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.GetTransaction(conn, "6SCQPcEvHHE2c34Hxse7PyxCdWHWYgZQhQpUNpw9314n", ctx)
+	response(resp, err)
+}
+
+func batchGetObjects(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.BatchGetObjects(conn, map[string]*uint64{
+		"0xe567f65413d10d585bbec909f46f11c5fc666061e1ede471b840f3d1cf25eaaa": nil,
+		"0x491d113c71512f45612e34eb2a25f7dd1014aa8bdb0718a22c4eea54f2a06340": nil,
+	}, ctx)
+	response(resp, err)
+}
+
+func batchGetTransactions(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.BatchGetTransactions(conn, []string{
+		"6SCQPcEvHHE2c34Hxse7PyxCdWHWYgZQhQpUNpw9314n",
+		"3WZieTbDv4CnBjbeBGs4AuakJAHySTEqXu6gu1bxY8rV",
+	}, ctx)
+	response(resp, err)
+}
+
+func getServiceInfo(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.GetServiceInfo(conn, ctx)
+	response(resp, err)
+}
+
+func getPackage(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.GetPackage(conn, "0xd84704c17fc870b8764832c535aa6b11f21a95cd6f5bb38a9b07d2cf42220c66", ctx)
+	response(resp, err)
+}
+
+func getFunction(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.GetFunction(conn, "0xd84704c17fc870b8764832c535aa6b11f21a95cd6f5bb38a9b07d2cf42220c66", "system", "reserve_space", ctx)
+	response(resp, err)
+}
+
+func getDatatype(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.GetDatatype(conn, "0xd84704c17fc870b8764832c535aa6b11f21a95cd6f5bb38a9b07d2cf42220c66", "system", "System", ctx)
+	response(resp, err)
+}
+
+func listPackageVersions(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.ListPackageVersions(conn, "0xd84704c17fc870b8764832c535aa6b11f21a95cd6f5bb38a9b07d2cf42220c66", nil, nil, ctx)
+	response(resp, err)
+}
+
+func getBalance(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.GetBalance(conn, "0x8aeec8403b86f22e58d87bdc85ff78c87b69dce58b8f651900b9eb5644f45180", "0x8270feb7375eee355e64fdb69c50abb6b5f9393a722883c1cf45f8e26048810a::wal::WAL", ctx)
+	response(resp, err)
+}
+
+func getCoinInfo(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.GetCoinInfo(conn, "0x8270feb7375eee355e64fdb69c50abb6b5f9393a722883c1cf45f8e26048810a::wal::WAL", ctx)
+	response(resp, err)
+}
+
+func listBalances(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.ListBalances(conn, "0x8aeec8403b86f22e58d87bdc85ff78c87b69dce58b8f651900b9eb5644f45180", nil, nil, ctx)
+	response(resp, err)
+}
+
+func listOwnedObjects(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.ListOwnedObjects(conn, "0x8aeec8403b86f22e58d87bdc85ff78c87b69dce58b8f651900b9eb5644f45180", nil, nil, ctx)
+	response(resp, err)
+}
+
+func listDynamicFields(conn *grpc.ClientConn, ctx context.Context) {
+	resp, err := gosuisdk.ListDynamicFields(conn, "0x6c2547cbbc38025cf3adac45f63cb0a8d12ecf777cdc75a4971612bf97fdf6af", nil, nil, ctx)
+	response(resp, err)
 }
---
+++ sui.go
@@ -0,0 +1,230 @@
+package gosuisdk
+
+import (
+	"context"
+	"fmt"
+
+	pb "github.com/pictorx/go-sui-sdk/sui_rpc_proto/generated"
+	"google.golang.org/grpc"
+)
+
+func GetEpoch(conn *grpc.ClientConn, ctx context.Context) (*pb.GetEpochResponse, error) {
+	client := pb.NewLedgerServiceClient(conn)
+	resp, err := client.GetEpoch(ctx, &pb.GetEpochRequest{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, err
+}
+
+func GetServiceInfo(conn *grpc.ClientConn, ctx context.Context) (*pb.GetServiceInfoResponse, error) {
+	client := pb.NewLedgerServiceClient(conn)
+	resp, err := client.GetServiceInfo(ctx, &pb.GetServiceInfoRequest{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func GetObject(conn *grpc.ClientConn, objectId string, version *uint64, ctx context.Context) (*pb.GetObjectResponse, error) {
+	client := pb.NewLedgerServiceClient(conn)
+	resp, err := client.GetObject(ctx, &pb.GetObjectRequest{
+		ObjectId: &objectId,
+		Version:  version,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, err
+}
+
+func GetTransaction(conn *grpc.ClientConn, digest string, ctx context.Context) (*pb.GetTransactionResponse, error) {
+	client := pb.NewLedgerServiceClient(conn)
+	resp, err := client.GetTransaction(ctx, &pb.GetTransactionRequest{
+		Digest: &digest,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func BatchGetObjects(conn *grpc.ClientConn, objects map[string]*uint64, ctx context.Context) (*pb.BatchGetObjectsResponse, error) {
+	if len(objects) == 0 {
+		return nil, fmt.Errorf("objects cannot be zero")
+	}
+
+	client := pb.NewLedgerServiceClient(conn)
+	ObjectRequests := []*pb.GetObjectRequest{}
+
+	for objectId, version := range objects {
+		ObjectRequests = append(ObjectRequests, &pb.GetObjectRequest{
+			ObjectId: &objectId,
+			Version:  version,
+		})
+	}
+
+	resp, err := client.BatchGetObjects(ctx, &pb.BatchGetObjectsRequest{
+		Requests: ObjectRequests,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, err
+}
+
+func BatchGetTransactions(conn *grpc.ClientConn, digests []string, ctx context.Context) (*pb.BatchGetTransactionsResponse, error) {
+	if len(digests) == 0 {
+		return nil, fmt.Errorf("digests parameter cannot be empty")
+	}
+
+	client := pb.NewLedgerServiceClient(conn)
+	resp, err := client.BatchGetTransactions(ctx, &pb.BatchGetTransactionsRequest{
+		Digests: digests,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, err
+}
+
+func GetPackage(conn *grpc.ClientConn, packageId string, ctx context.Context) (*pb.GetPackageResponse, error) {
+	client := pb.NewMovePackageServiceClient(conn)
+	resp, err := client.GetPackage(ctx, &pb.GetPackageRequest{
+		PackageId: &packageId,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func GetFunction(conn *grpc.ClientConn, packageId, module, funcName string, ctx context.Context) (*pb.GetFunctionResponse, error) {
+	client := pb.NewMovePackageServiceClient(conn)
+	resp, err := client.GetFunction(ctx, &pb.GetFunctionRequest{
+		PackageId:  &packageId,
+		ModuleName: &module,
+		Name:       &funcName,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, err
+}
+
+func GetDatatype(conn *grpc.ClientConn, packageId, module, dataTypeName string, ctx context.Context) (*pb.GetDatatypeResponse, error) {
+	client := pb.NewMovePackageServiceClient(conn)
+
+	resp, err := client.GetDatatype(ctx, &pb.GetDatatypeRequest{
+		PackageId:  &packageId,
+		ModuleName: &module,
+		Name:       &dataTypeName,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func ListPackageVersions(conn *grpc.ClientConn, packageId string, pagesize *uint32, pagetoken []byte, ctx context.Context) (*pb.ListPackageVersionsResponse, error) {
+	client := pb.NewMovePackageServiceClient(conn)
+
+	resp, err := client.ListPackageVersions(ctx, &pb.ListPackageVersionsRequest{
+		PackageId: &packageId,
+		PageSize:  pagesize,
+		PageToken: pagetoken,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, err
+}
+
+func GetBalance(conn *grpc.ClientConn, owner, cointype string, ctx context.Context) (*pb.GetBalanceResponse, error) {
+	client := pb.NewStateServiceClient(conn)
+	resp, err := client.GetBalance(ctx, &pb.GetBalanceRequest{
+		Owner:    &owner,
+		CoinType: &cointype,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func GetCoinInfo(conn *grpc.ClientConn, cointype string, ctx context.Context) (*pb.GetCoinInfoResponse, error) {
+	client := pb.NewStateServiceClient(conn)
+	resp, err := client.GetCoinInfo(ctx, &pb.GetCoinInfoRequest{
+		CoinType: &cointype,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func ListBalances(conn *grpc.ClientConn, owner string, pagesize *uint32, pagetoken []byte, ctx context.Context) (*pb.ListBalancesResponse, error) {
+	client := pb.NewStateServiceClient(conn)
+	resp, err := client.ListBalances(ctx, &pb.ListBalancesRequest{
+		Owner:     &owner,
+		PageSize:  pagesize,
+		PageToken: pagetoken,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func ListOwnedObjects(conn *grpc.ClientConn, owner string, pagesize *uint32, pagetoken []byte, ctx context.Context) (*pb.ListOwnedObjectsResponse, error) {
+	client := pb.NewStateServiceClient(conn)
+	resp, err := client.ListOwnedObjects(ctx, &pb.ListOwnedObjectsRequest{
+		Owner:     &owner,
+		PageSize:  pagesize,
+		PageToken: pagetoken,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func ListDynamicFields(conn *grpc.ClientConn, objectId string, pagesize *uint32, pagetoken []byte, ctx context.Context) (*pb.ListDynamicFieldsResponse, error) {
+	client := pb.NewStateServiceClient(conn)
+	resp, err := client.ListDynamicFields(ctx, &pb.ListDynamicFieldsRequest{
+		Parent:    &objectId,
+		PageSize:  pagesize,
+		PageToken: pagetoken,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
